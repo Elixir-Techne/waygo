d374afedc9e25e4edaa7f930e9a24889420540d1
Elixir Techne <itsupport@elixirtechne.com>
2023-07-18 16:29:39 +0530

    Add status report card.

--- frontend/src/pages/statusReports/index.js
+++ frontend/src/pages/statusReports/index.js
@@ -1,7 +1,25 @@
 import React from "react";
+import ArgonBox from "components/ArgonBox";
+import StatusReportCard from "./components/StatusReportCard";
+import { Endpoints } from "utils/httpServices";
+import { useQuery } from "@tanstack/react-query";
 
 const StatusReports = () => {
-  return <div>StatusReports</div>;
+  const { data, isLoading } = useQuery([Endpoints.statusReport], {
+    enabled: true,
+  });
+  return (
+    <ArgonBox
+      mt={8}
+      display="flex"
+      flexDirection="row"
+      flexWrap="wrap"
+      gap="20px"
+    >
+      {!isLoading &&
+        data?.map((item) => <StatusReportCard data={item} key={item.id} />)}
+    </ArgonBox>
+  );
 };
 
 export default StatusReports;
